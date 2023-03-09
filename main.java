d4a0ab4eb291595d69bc48e2308296a23b224611
EC2 Default User <ec2-user@ip-172-31-9-176.ap-northeast-1.compute.internal>
2023-03-09 07:05:16 +0000

    java1

---
+++ main.java
@@ -0,0 +1,43 @@
+class Main {
+  public static void main(String[] args) {
+    //System.out.println("Hello Java");
+    //System.out.println(17);
+    // System.out.println(12 / 3);
+    // System.out.println(3 * 6);
+    // System.out.println(8 % 3);
+    // System.out.println("こんにちは" + "世界");
+    // System.out.println("38" + "19");
+    // int number;
+    // number = 3;
+    // System.out.println(number);
+    
+    // String name;
+    // name = "Wanko";
+    // System.out.println(name);
+    
+    // int number2 = 7;
+    // System.out.println(number1 * number2);
+    
+    // String text = "プログラミングを勉強しよう";
+    // System.out.println("Progateで" + text);
+    
+    // number *= 7;
+    // System.out.println(number);
+    // number++;
+    // System.out.println(number);
+    // double number1 = 8.5;
+    // double number2 = 3.4;
+    // System.out.println((double)number1 / number2);
+    // String name = "アンナ";
+    // int age = 24;
+    // int height = 165;
+    // int weight = 50;
+    // double bmi = weight / height / height;
+    // System.out.println(name);
+    // System.out.println(age);
+    // System.out.println(height);
+    // System.out.println(weight);
+    // System.out.println(bmi);
+    
+   }
+}
